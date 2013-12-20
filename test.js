18ac74b3a27a090566d43835abeb962bd245200f
Marius A. <marius.anderie@hotmail.com>
2013-12-20 11:17:33 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 
 var canv = document.getElementById("canv");
 var ctx = canv.getContext("2d");
-var node_size = 200;
+var node_size = 50;
 
 
 var nodes = new Array();
@@ -9,7 +9,42 @@ var nodes = new Array();
 for (var i = 0; i < canv.height/node_size; i ++) {
     nodes[i] = new Array();
     for (var n = 0; n < canv.width/node_size; n++) {
-        nodes[i][n] = Math.floor(Math.random()*1000) > 500 ? "blue" : "brown";
+        nodes[i][n] = new Array();
+        
+        //set color
+        nodes[i][n][0] = Math.floor(Math.random()*1000) > 500 ? "blue" : "brown";
+        
+        switch(nodes[i][n][0]) {
+            case "brown":
+                //Genmaterial
+                //braun
+                nodes[i][n][1] = 100;
+                //blau
+                nodes[i][n][2] = 0;
+                //grün
+                nodes[i][n][3] = 0;
+                break;
+                
+            case "blue": 
+                //Genmaterial
+                //braun
+                nodes[i][n][1] = 0;
+                //blau
+                nodes[i][n][2] = 100;
+                //grün
+                nodes[i][n][3] = 0;
+                break;
+                
+            case "grey": 
+                //Genmaterial
+                //braun
+                nodes[i][n][1] = 0;
+                //blau
+                nodes[i][n][2] = 0;
+                //grün
+                nodes[i][n][3] = 100;
+                break;
+        }
     }
 }
 
@@ -26,7 +61,7 @@ for (var i = 0; i < canv.height/node_size; i ++) {
 for (var i = 0; i < canv.height/node_size; i ++) {
     for (var n = 0; n < canv.width/node_size; n++) {
         
-        switch(nodes[i][n]) {
+        switch(nodes[i][n][0]) {
             case "brown": ctx.fillStyle = "#6B1B00"; break;
             case "blue": ctx.fillStyle = "#00ACE6"; break;
             case "grey": ctx.fillStyle = "#D6D6D6"; break;
